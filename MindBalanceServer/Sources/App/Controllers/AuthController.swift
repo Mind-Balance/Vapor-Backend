7416faaf019f32b046ba056a773bf2601e231224
Salva Moreno <morenosanchezsalva@gmail.com>
2024-04-13 18:02:36 +0200

    :coffin: Remove sign up implementation

--- MindBalanceServer/Sources/App/Controllers/AuthController.swift
+++ MindBalanceServer/Sources/App/Controllers/AuthController.swift
@@ -11,7 +11,6 @@ import Fluent
 struct AuthController: RouteCollection {
     func boot(routes: Vapor.RoutesBuilder) throws {
         routes.group("auth") { builder in
-            builder.post("signup", use: signUp)
             builder.group(User.authenticator(), User.guardMiddleware()) { builder in
                 builder.get("signin", use: signIn)
             }
@@ -23,26 +22,6 @@ struct AuthController: RouteCollection {
 }
 
 extension AuthController {
-    func signUp(req: Request) async throws -> JWTToken.Public {
-        // Validate content entry
-        try User.Create.validate(content: req)
-        
-        // Decode user data
-        let userCreate = try req.content.decode(User.Create.self)
-        let passwordHashed = try req.password.hash(userCreate.password)
-        
-        // Save user to DB
-        let user = User(name: userCreate.name, email: userCreate.email, password: passwordHashed)
-        try await user.create(on: req.db)
-        
-        // Generate tokens
-        let tokens = JWTToken.generateToken(userID: user.id!)
-        let accessSigned = try req.jwt.sign(tokens.accessToken)
-        let refreshSigned = try req.jwt.sign(tokens.refreshToken)
-        
-        return JWTToken.Public(accessToken: accessSigned, refreshToken: refreshSigned)
-    }
-    
     func signIn(req: Request) async throws -> JWTToken.Public {
         // Get authenticated user
         let user = try req.auth.require(User.self)
---
+++ MindBalanceServer/Sources/App/Migrations/PopulateInitialData_v0.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Salva Moreno on 13/4/24.
+//
+
+import Vapor
+import Fluent
+
+struct PopulateInitialData_v0: AsyncMigration {
+    func prepare(on database: Database) async throws {
+        
+    }
+    
+    func revert(on database: Database) async throws {
+        
+    }
+}
